42ac697516edefd65bd3af3f123987a1030d1b60
Maciej Palmowski <m.palmowski@freshpixels.pl>
2021-02-24 14:11:36 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,7 @@ module.exports = {
         // the only required plugin option for WordPress is the GraphQL url.
         url:
           process.env.WPGRAPHQL_URL ||
-          `https://sandbox-test.live.page`,
+          `https://sandbox-test.live.page/graphql`,
       },
     },
 
